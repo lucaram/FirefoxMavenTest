a002f912f7a2e68424a291b2ae5756f4a6352238
lramirez <lramirez@FX-TST-21.Formedix.local>
2014-01-08 12:10:06 +0000

    FirefoxMavenTest

--- src/test/java/MyTest/AppNGTest.java
+++ src/test/java/MyTest/AppNGTest.java
@@ -8,17 +8,17 @@ import org.testng.annotations.*;
 
 public class AppNGTest {
     //public String baseUrl = "http://newtours.demoaut.com/";
-    ksjhspublic String baseUrl = "http://www.aimargini.com";
+    public String baseUrl = "http://www.aimargini.com";
     public WebDriver driver = new FirefoxDriver();
     
     
     @Test
     public void verifyHomepageTitle() throws InterruptedException {
         driver.get(baseUrl);
-        Thread.sleep(5000);
-        //String expectedTitle = "Welcome: Mercury Tours";
-        //String actualTitle = driver.getTitle();
-        //Assert.assertEquals(actualTitle, expectedTitle);
+        Thread.sleep(3000);
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
         driver.quit();
     }
 }
