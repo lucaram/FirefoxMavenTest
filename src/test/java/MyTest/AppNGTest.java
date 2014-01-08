dd966f462c6c6d630e0bcdddb2c9ca295214ca58
lramirez <lramirez@FX-TST-21.Formedix.local>
2014-01-08 13:33:28 +0000

    FirefoxMavenTest

--- src/test/java/MyTest/AppNGTest.java
+++ src/test/java/MyTest/AppNGTest.java
@@ -16,7 +16,7 @@ public class AppNGTest {
     public void verifyHomepageTitle() {
         driver.get(baseUrl);
         //Thread.sleep(3000);
-        String expectedTitle = "Alkjhsflksd.,i Margini";
+        String expectedTitle = "Ai Margini";
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle, expectedTitle);
     }
