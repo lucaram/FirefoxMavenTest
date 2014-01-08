17e0ef38bb378e02c302549463be8ef5dcb06b6a
lramirez <lramirez@FX-TST-21.Formedix.local>
2014-01-08 09:52:01 +0000

    FirefoxMavenTest

--- src/test/java/MyTest/AppNGTest.java
+++ src/test/java/MyTest/AppNGTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class AppNGTest {
+apublic class AppNGTest {
     public String baseUrl = "http://newtours.demoaut.com/";
     public WebDriver driver = new FirefoxDriver();
     
